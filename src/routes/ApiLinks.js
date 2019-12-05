fd613f0f02ccce0b943b8ae4b2adcc0546dade1a
Chinonso Eke <Chinonsoeke@gmail.com>
2019-12-05 23:56:46 +0100

    Mods to css

--- src/routes/ApiLinks.js
+++ src/routes/ApiLinks.js
@@ -1,4 +1,4 @@
-const server = window.location.origin === 'http://localhost:3000' ? 'live' : 'live'
+const server = window.location.origin === 'http://localhost:3000' ? 'local' : 'live'
 export const endpoint = server === 'local' ? 'http://127.0.0.1:8000' : 'https://api-renworld-bank.herokuapp.com'
 export const api = `${endpoint}/api`
 
