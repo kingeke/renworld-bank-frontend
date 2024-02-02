4651de2944717b2588dd97bbf0184f7a7aa10cfb
Chinonso Eke <ceke@renmoney.com>
2024-02-02 17:01:31 +0100

    updates to api

--- src/routes/ApiLinks.js
+++ src/routes/ApiLinks.js
@@ -1,6 +1,6 @@
 const server = window.location.origin === 'http://localhost:3000' ? 'local' : 'live'
-export const endpoint = server === 'local' ? 'http://127.0.0.1:8000' : 'https://api-renworld-bank.herokuapp.com'
-export const api = `${endpoint}/api`
+export const endpoint = server === 'local' ? 'http://127.0.0.1:8000' : 'https://renworld-bank-backend.vercel.app'
+export const api = `${endpoint}/backend`
 
 export const apiLinks = {
     website: `${api}/website`,
