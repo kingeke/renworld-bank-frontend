a9737fa13e9b832a90d5b20423e7d0e862812d7a
Chinonso Eke <ceke@renmoney.com>
2024-02-02 17:14:20 +0100

    debug

---
+++ build/precache-manifest.81457510a6fcc43b3e52b6322a2c3cc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1950d364489b8e0a8021f75adbcb31af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71a0b5696f8b385b771d",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "76baddf18d6f97f0afe1",
+    "url": "/static/css/main.2a254a78.chunk.css"
+  },
+  {
+    "revision": "71a0b5696f8b385b771d",
+    "url": "/static/js/2.94102208.chunk.js"
+  },
+  {
+    "revision": "e30960b8e7a04ac42506ea0f9f90da53",
+    "url": "/static/js/2.94102208.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76baddf18d6f97f0afe1",
+    "url": "/static/js/main.0bbc3704.chunk.js"
+  },
+  {
+    "revision": "b4740eb520542b128a00",
+    "url": "/static/js/runtime-main.6452cf35.js"
+  }
+]);
